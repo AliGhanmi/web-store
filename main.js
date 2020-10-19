0702bdab763a699308f71a9cfb32599f2ed16526
ali010143 <ghanmiali7@gmail.com>
2020-10-19 19:20:39 +0200

    nearly finished

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
-function items(title,image,description, button){
+function items(title,image,description, button,gender){
     var item = {}
-    item.title=title,
-    item.image=image,
+    item.title=title
+    item.image=image
     item.description=description
     item.button=button
+    item.gender=gender
     return item
 }
-var item1=items('Winter-Jacket', 'https://ae01.alicdn.com/kf/Hb4c99f279b64485d9ab1dc93003c5e22A/2020-Alpinestars-Winter-Jacket-Men-Fashion-Stand-Collar-Male-Parka-Jacket-Mens-Solid-Thick-Jackets-and.jpg_220x220xz.jpg_.webp','Price:17$','Add To List')
-var item2 =items('Hoodie-Trouser','https://ae01.alicdn.com/kf/H63ed512cffbf4c50a831e25d61ddcf708/2020-hot-sale-autumn-and-winter-new-brand-men-s-suit-casual-sports-fleece-font-b.jpg_220x220xz.jpg_.webp','Price:14$','Add To List')
-var item3=items('T-Shirt','https://ae01.alicdn.com/kf/H140aab3edae742e4b0310f570b8f3fd9v/Men-Polo-Men-Shirt-Short-Sleeve-Polo-Shirt-Contrast-Color-Polo-New-Clothing-Summer-Streetwear-Casual.jpg_220x220xz.jpg_.webp','Price:20$','Add To List')
-var item4=items('Wedding-Suit','https://ae01.alicdn.com/kf/Hfbcb8e7e10bf485fa2770f0ee98b89deo/New-Fashion-Handsome-Dark-Gray-Mens-Suit-Groom-Suit-Wedding-Suits-For-Best-Men-Slim-Fit.jpg_220x220xz.jpg_.webp','Price:150$','Add To List')
-var item5=items('Shoes','https://ae01.alicdn.com/kf/H7b9df92791df4348923b2468f8425a08z/Leather-Men-Shoes-Luxury-Brand-England-Trend-Casual-Shoes-Men-Sneakers-Italian-Breathable-Leisure-Male-Footwear.jpg_220x220xz.jpg_.webp','Price:50$','Add To List')
-var item6=items('Female-T-shirt','https://ae01.alicdn.com/kf/Hf0444c32308544bf9330218978e18d79K/Female-T-shirt-cartoon-PAZ-Mafalda-or-QUIERO-Cafe-printed-female-graphic-T-shirt-Harajuku-funny.jpg_220x220xz.jpg_.webp','Price:10$','Add To List')
-var item7=items('Female-Jeans-Jacket','https://ae01.alicdn.com/kf/HTB1u1K4czbguuRkHFrdq6z.LFXaD/2020-Winter-Jeans-Jackets-Plus-Velvet-Warm-Parka-Outerwear-Women-Cashmere-Cotton-Coat-Girl-Fashion-Denim.jpg_220x220xz.jpg_.webp','Price:30$','Add To List')
-var item8=items('Mini-Dress','https://ae01.alicdn.com/kf/H664b6460e9da4c828e7b835a1db1d352v/Simplee-Casual-suit-collar-autumn-women-dress-Long-sleeve-office-lady-asymmetrical-long-dress-Slim-with.jpg_220x220xz.jpg_.webp','Price:50$','Add To List')
-var item9=items('Skirt','https://ae01.alicdn.com/kf/H79a6a84a6ed0461dbb120ddc046dda14a/Neploe-High-Waist-Hip-Women-Skirt-Solid-A-line-Pocket-Jupe-Femme-Autumn-Winter-Wild-Boycon.jpg_220x220xz.jpg_.webp','Price:40$','Add To List')
-var item10=items('Female-Trousres','https://ae01.alicdn.com/kf/H75e26e9a252941c5add01050ba27dba16/Korean-High-Waist-Jeans-Women-Harem-Pants-Loose-Casual-Plus-Size-High-Street-Denim-Trousers-Pantalon.jpg_220x220xz.jpg_.webp','Price:30$','Add To List')
-var item11=items('Slippers','https://ae01.alicdn.com/kf/H08ef43567ee64ef09040ad0c4539d8836/slippers-men-Summer-Shoes-Sandals-Male-Slipper-Indoor-Or-Outdoor-Flip-Flops-indoor-shoes-zapatos-de.jpg_220x220xz.jpg_.webp','Price:5$','Add To List')
-var item12=items('Jeans','https://ae01.alicdn.com/kf/H090e4d5aca8446ceb6b858e3261796b6o/-font-b-Mens-b-font-Streetwear-font-b-Jeans-b-font-Destroyed-Ripped-Design-Pencil.jpg_220x220xz.jpg_.webp','Price:10$','Add To List')
-var item13=items('Swimming-Shorts','https://ae01.alicdn.com/kf/H325b233e92274fbe858e6005999965310/Men-s-Swim-Briefs-3D-print-Bikini-Swimwear-Blue-Fish-Men-Swimming-Trunks-Surf-Beach-Pant.jpg_220x220xz.jpg_.webp','Price:8$','Add To List')
-var item14=items('Winter-Sweater','https://ae01.alicdn.com/kf/H106e2d32dd0d43a88db7baccf718e20ck/Men-Autumn-Sweater-Coat-Thick-Fashion-Casual-Sweater-Cardigan-Men-Brand-Slim-Fit-Knitwear-Outerwear-Warm.jpg_220x220xz.jpg_.webp','Price:80$','Add To List')
-var item15=items('Shoes-wedding-Business','https://ae01.alicdn.com/kf/Hf2f030a4b12f44e28899877a8c7b20a0q/fashion-Mens-leather-shoes-wedding-Business-dress-Nightclubs-oxfords-Breathable-Working-lace-up-shoes-345rt.jpg_220x220xz.jpg_.webp','Price:60$','Add To List')
-var item16=items('Sandals-Wedding','https://ae01.alicdn.com/kf/Hfdd1716eaff643dcb07277cc5ed81ea5c/2020-Gladiator-Women-Pumps-Ladies-Sexy-Buckle-Strap-Roman-High-Heels-Open-Toe-Sandals-Party-Wedding.jpg_220x220xz.jpg_.webp','Price:26$','Add To List')
-var item17=items('Stripper-Heels','https://ae01.alicdn.com/kf/H6fd5df53ec894a228282a2ad0b6e41bcs/Stripper-font-b-Heels-b-font-Women-Shoes-2020-Summer-Platform-font-b-High-b-font.jpg_220x220xz.jpg_.webp','Price:24$','Add To List')
-var item18=items('Boots','https://ae01.alicdn.com/kf/Hfd12befcb8fa4aaabf1b3e2d6164f15aH/Women-Over-The-Knee-Winter-font-b-Boots-b-font-wool-fur-Leather-Belt-font-b.jpg_220x220xz.jpg_.webp','Price:29$','Add To List')
-var item19=items('Female-Shoes','https://ae01.alicdn.com/kf/H62330fc5795d414ebbfcbb282ca79a95S/Women-Colorful-Cool-Sneaker-Ladies-Lace-Up-Vulcanized-Shoes-Casual-Female-Flat-Comfort-Walking-Shoes-Woman.jpg_220x220xz.jpg_.webp','Price:30$','Add To List')
-var item20=items('Female-Slippers','https://ae01.alicdn.com/kf/H930d444e49a448818cd8fc11414dd8d8Z/-font-b-Female-b-font-house-font-b-slippers-b-font-women-s-platform-chunky.jpg_220x220xz.jpg_.webp','Price:10$','Add To List')
+var item1=items('Winter-Jacket', 'https://ae01.alicdn.com/kf/Hb4c99f279b64485d9ab1dc93003c5e22A/2020-Alpinestars-Winter-Jacket-Men-Fashion-Stand-Collar-Male-Parka-Jacket-Mens-Solid-Thick-Jackets-and.jpg_220x220xz.jpg_.webp','Price:17$','Add To List','men')
+var item2 =items('Hoodie-Trouser','https://ae01.alicdn.com/kf/H63ed512cffbf4c50a831e25d61ddcf708/2020-hot-sale-autumn-and-winter-new-brand-men-s-suit-casual-sports-fleece-font-b.jpg_220x220xz.jpg_.webp','Price:14$','Add To List','men')
+var item3=items('T-Shirt','https://ae01.alicdn.com/kf/H140aab3edae742e4b0310f570b8f3fd9v/Men-Polo-Men-Shirt-Short-Sleeve-Polo-Shirt-Contrast-Color-Polo-New-Clothing-Summer-Streetwear-Casual.jpg_220x220xz.jpg_.webp','Price:20$','Add To List','men')
+var item4=items('Wedding-Suit','https://ae01.alicdn.com/kf/Hfbcb8e7e10bf485fa2770f0ee98b89deo/New-Fashion-Handsome-Dark-Gray-Mens-Suit-Groom-Suit-Wedding-Suits-For-Best-Men-Slim-Fit.jpg_220x220xz.jpg_.webp','Price:150$','Add To List','men')
+var item5=items('Shoes','https://ae01.alicdn.com/kf/H7b9df92791df4348923b2468f8425a08z/Leather-Men-Shoes-Luxury-Brand-England-Trend-Casual-Shoes-Men-Sneakers-Italian-Breathable-Leisure-Male-Footwear.jpg_220x220xz.jpg_.webp','Price:50$','Add To List','men')
+var item6=items('Female-T-shirt','https://ae01.alicdn.com/kf/Hf0444c32308544bf9330218978e18d79K/Female-T-shirt-cartoon-PAZ-Mafalda-or-QUIERO-Cafe-printed-female-graphic-T-shirt-Harajuku-funny.jpg_220x220xz.jpg_.webp','Price:10$','Add To List','women')
+var item7=items('Female-Jeans-Jacket','https://ae01.alicdn.com/kf/HTB1u1K4czbguuRkHFrdq6z.LFXaD/2020-Winter-Jeans-Jackets-Plus-Velvet-Warm-Parka-Outerwear-Women-Cashmere-Cotton-Coat-Girl-Fashion-Denim.jpg_220x220xz.jpg_.webp','Price:30$','Add To List','women')
+var item8=items('Mini-Dress','https://ae01.alicdn.com/kf/H664b6460e9da4c828e7b835a1db1d352v/Simplee-Casual-suit-collar-autumn-women-dress-Long-sleeve-office-lady-asymmetrical-long-dress-Slim-with.jpg_220x220xz.jpg_.webp','Price:50$','Add To List','women')
+var item9=items('Skirt','https://ae01.alicdn.com/kf/H79a6a84a6ed0461dbb120ddc046dda14a/Neploe-High-Waist-Hip-Women-Skirt-Solid-A-line-Pocket-Jupe-Femme-Autumn-Winter-Wild-Boycon.jpg_220x220xz.jpg_.webp','Price:40$','Add To List','women')
+var item10=items('Female-Trousres','https://ae01.alicdn.com/kf/H75e26e9a252941c5add01050ba27dba16/Korean-High-Waist-Jeans-Women-Harem-Pants-Loose-Casual-Plus-Size-High-Street-Denim-Trousers-Pantalon.jpg_220x220xz.jpg_.webp','Price:30$','Add To List','women')
+var item11=items('Slippers','https://ae01.alicdn.com/kf/H08ef43567ee64ef09040ad0c4539d8836/slippers-men-Summer-Shoes-Sandals-Male-Slipper-Indoor-Or-Outdoor-Flip-Flops-indoor-shoes-zapatos-de.jpg_220x220xz.jpg_.webp','Price:5$','Add To List','men')
+var item12=items('Jeans','https://ae01.alicdn.com/kf/H090e4d5aca8446ceb6b858e3261796b6o/-font-b-Mens-b-font-Streetwear-font-b-Jeans-b-font-Destroyed-Ripped-Design-Pencil.jpg_220x220xz.jpg_.webp','Price:10$','Add To List','men')
+var item13=items('Swimming-Shorts','https://ae01.alicdn.com/kf/H325b233e92274fbe858e6005999965310/Men-s-Swim-Briefs-3D-print-Bikini-Swimwear-Blue-Fish-Men-Swimming-Trunks-Surf-Beach-Pant.jpg_220x220xz.jpg_.webp','Price:8$','Add To List','men')
+var item14=items('Winter-Sweater','https://ae01.alicdn.com/kf/H106e2d32dd0d43a88db7baccf718e20ck/Men-Autumn-Sweater-Coat-Thick-Fashion-Casual-Sweater-Cardigan-Men-Brand-Slim-Fit-Knitwear-Outerwear-Warm.jpg_220x220xz.jpg_.webp','Price:80$','Add To List','men')
+var item15=items('Shoes-wedding-Business','https://ae01.alicdn.com/kf/Hf2f030a4b12f44e28899877a8c7b20a0q/fashion-Mens-leather-shoes-wedding-Business-dress-Nightclubs-oxfords-Breathable-Working-lace-up-shoes-345rt.jpg_220x220xz.jpg_.webp','Price:60$','Add To List','men')
+var item16=items('Sandals-Wedding','https://ae01.alicdn.com/kf/Hfdd1716eaff643dcb07277cc5ed81ea5c/2020-Gladiator-Women-Pumps-Ladies-Sexy-Buckle-Strap-Roman-High-Heels-Open-Toe-Sandals-Party-Wedding.jpg_220x220xz.jpg_.webp','Price:26$','Add To List','women')
+var item17=items('Stripper-Heels','https://ae01.alicdn.com/kf/H6fd5df53ec894a228282a2ad0b6e41bcs/Stripper-font-b-Heels-b-font-Women-Shoes-2020-Summer-Platform-font-b-High-b-font.jpg_220x220xz.jpg_.webp','Price:24$','Add To List','women')
+var item18=items('Boots','https://ae01.alicdn.com/kf/Hfd12befcb8fa4aaabf1b3e2d6164f15aH/Women-Over-The-Knee-Winter-font-b-Boots-b-font-wool-fur-Leather-Belt-font-b.jpg_220x220xz.jpg_.webp','Price:29$','Add To List','women')
+var item19=items('Female-Shoes','https://ae01.alicdn.com/kf/H62330fc5795d414ebbfcbb282ca79a95S/Women-Colorful-Cool-Sneaker-Ladies-Lace-Up-Vulcanized-Shoes-Casual-Female-Flat-Comfort-Walking-Shoes-Woman.jpg_220x220xz.jpg_.webp','Price:30$','Add To List','women')
+var item20=items('Female-Slippers','https://ae01.alicdn.com/kf/H930d444e49a448818cd8fc11414dd8d8Z/-font-b-Female-b-font-house-font-b-slippers-b-font-women-s-platform-chunky.jpg_220x220xz.jpg_.webp','Price:10$','Add To List','women')
 var array = [item1,item2,item3,item4,item5,item6,item7,item8,item9,item10,item11,item12,item13,item14,item15,item16,item17,item18,item19,item20]
 
 
@@ -35,7 +36,6 @@ function model(item){
     $('#'+item.title+'').append("<img src='"+item.image+"'></img>")
     $('#'+item.title+'').append('<p>'+item.description+'</p>')
     $('#'+item.title+'').append('<button class="btn">'+item.button+'</button>')
-    console.log($('.cards_men'))
 }
 
 function models(array){
@@ -44,3 +44,14 @@ function models(array){
     }
 }
 models(array)
+
+function test(array,query){
+    $('#main').html('')
+    for (var i= 0; i<array.length; i++){
+        if(array[i].gender===query){
+            model(array[i])
+        }else if(!query) {
+            model(array[i])
+        }
+    }    
+}
