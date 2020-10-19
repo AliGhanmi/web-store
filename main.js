f012c3413e00324d21312c8eaa69f84f6426a96e
ali010143 <ghanmiali7@gmail.com>
2020-10-19 13:00:52 +0200

    trying to make data modeling

---
+++ main.js
@@ -0,0 +1,8 @@
+function items(title,image,description){
+    var item = {}
+    item.title=title,
+    item.image=image,
+    item.description=description
+    return item
+}
+var item1=('Winter Jacket', '')
